7525c0ea7122d08a1e43746a261a36538614baa6
i@pluvet.com <pluveto>
2022-03-22 09:35:23 +0800

    fix: add cd command

--- start.example.sh
+++ start.example.sh
@@ -1,3 +1,4 @@
 export DNSPOD_TOKEN_ID=123456
 export DNSPOD_TOKEN=12345c555551effec3cb06d555554444
+cd /root/app/ddns
 python3 ddns_dnspod.py --domain example.com --sub-domain `hostname` 2>&1 | tee -a ddns.log
